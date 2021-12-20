7a1939cdc97d9f61059b5657c256d4eafd3da1f1
Aicha Macoura Koné <aicha.kone@odc.local>
2021-12-20 15:23:31 +0000

    first commit

---
+++ src/test/java/com/GroupeRepartition/GroupeRepartition/GroupeRepartitionApplicationTests.java
@@ -0,0 +1,13 @@
+package com.GroupeRepartition.GroupeRepartition;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class GroupeRepartitionApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
